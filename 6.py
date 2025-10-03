839b001200459c8c60823c647c91c5526249d5d1
Sadananda <sadanandaw13@gmail.com>
2025-10-03 22:00:26 +0530

    Class Object

---
+++ 6.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self,brand,model,year):
+        self.brand=brand
+        self.model=model
+        self.year=year
+
+Carobj=Car("Audi","V1","2015")
+print(Carobj.brand,Carobj.model,Carobj.year)
+
